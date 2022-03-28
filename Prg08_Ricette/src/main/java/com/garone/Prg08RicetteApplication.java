c9ec6ed09629185b78ecb11dd59bd10d9968a2a5
Giuseppe <96467406+ggarone@users.noreply.github.com>
2022-03-28 09:15:31 +0200

    Add files via upload

---
+++ Prg08_Ricette/src/main/java/com/garone/Prg08RicetteApplication.java
@@ -0,0 +1,13 @@
+package com.garone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prg08RicetteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prg08RicetteApplication.class, args);
+	}
+
+}
